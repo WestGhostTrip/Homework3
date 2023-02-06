541c729b743f42528957bd3d4c86ad264c2b18c2
WestGhostTrip <98178922+WestGhostTrip@users.noreply.github.com>
2023-02-06 20:00:15 +0300

    Update Task.js

--- Task.js
+++ Task.js
@@ -1,13 +1,44 @@
+
+"use strict";
 function getArrayParams(arr) {
-  let min,
-    max,
-    sum = arr[0];
-  for (let index = 1; index < arr.length; index++) {
-    const element = arr[index];
-    if (min > element) min = element;
-    if (max < element) max = element;
-    sum += element;
+  let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max)
+      max = arr[i];
+    if (arr[i] < min)
+      min = arr[i];
+    sum = arr[i] + sum;
   }
-  const avg = +(sum / arr.length).toFixed(2);
-  return { max, min, sum, avg };
+  avg = Number((sum / arr.length).toFixed(2));
+  return { min: min, max: max, avg: avg };
+}
+
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const calculatingElements = func(arrOfArr[i]);
+    if (calculatingElements > max) {
+      max = calculatingElements;
+    }
+  }
+
+  return max;
+}
+
+// Задание 3
+function worker2(arr) {
+  let result = getArrayParams(arr);
+  return Math.abs(result.max - result.min);
 }
